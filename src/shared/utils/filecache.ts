0479fa0a6fe2018c760c49f03fd0fb30b245a56b
ivangegovdve-sudo <ivangegov.dve@gmail.com>
2026-03-19 10:35:17 +0200

    perf: avoid blocking file cache operations (#39)

--- src/shared/utils/filecache.ts
+++ src/shared/utils/filecache.ts
@@ -1,4 +1,4 @@
-import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
+import { existsSync, mkdirSync, promises as fsPromises } from "fs";
 import { join } from "path";
 import { base64 } from "@shared/utils/base64";
 import { app } from "electron";
@@ -10,9 +10,9 @@ export async function cacheWithFile<T>(fn: () => Promise<T>, key: string): Promi
 	const enc = new Encryption({ secret: base64.encode(key) });
 	const cacheFile = join(cachePath, key + ".ytm");
 	if (existsSync(cacheFile)) {
-		return enc.decrypt(readFileSync(cacheFile, "utf8"));
+		return enc.decrypt(await fsPromises.readFile(cacheFile, "utf8"));
 	}
 	const result = (await fn()) as T;
-	writeFileSync(cacheFile, enc.encrypt(result));
+	await fsPromises.writeFile(cacheFile, enc.encrypt(result));
 	return result;
 }
